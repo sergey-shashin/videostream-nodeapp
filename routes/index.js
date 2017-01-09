be828b7e7f6aebf79a8cad55ae7dddfd9f2516b3
sergey-shashin <info@sergey-shashin.ru>
2017-01-09 13:18:22 +0300

    Nothig interesting was made
    learned jade and added bootstrap 4

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Video Share' });
+  res.render('index', { title: 'Webinar app' });
 });
 
 module.exports = router;
